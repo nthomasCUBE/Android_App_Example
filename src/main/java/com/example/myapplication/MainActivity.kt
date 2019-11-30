d5f6dbef121a9409e2df1415aa5e4c71a8e1043e
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-11-30 17:40:38 +0100

    Add files via upload

---
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.AutoCompleteTextView;
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var goToChildButton = findViewById<Button>(R.id.go_to_child_btn);
+        goToChildButton.setOnClickListener{
+
+            var prename = findViewById<AutoCompleteTextView>(R.id.tx1);
+            var lastname = findViewById<AutoCompleteTextView>(R.id.tx2);
+
+            Toast.makeText(this,prename.editableText.toString(),Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,lastname.editableText.toString(),Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
