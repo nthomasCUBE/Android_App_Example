e7b6c0a966abbe6c4d65dcb8a3aefda9c3197e84
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-03 06:55:38 +0100

    Add files via upload

--- src/main/java/com/example/myapplication/MainActivity.kt
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -31,9 +31,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var goToChildButton2 = findViewById<Button>(R.id.go_to_child_btn2);
+        goToChildButton2.setOnClickListener {
+
+            var editText = findViewById<EditText>(R.id.editText);
+3
+            var searchText = findViewById<AutoCompleteTextView>(R.id.tx3);
+            val databaseHandler: DatabaseHandler= DatabaseHandler(this)
+            val emp:List<PersonModelClass> = databaseHandler.viewPerson()
+
+            val x=StringBuilder()
+            for(e in emp){
+                if(e.lastname.toString().equals(searchText.editableText.toString()) or e.prename.toString().equals(searchText.editableText.toString())){
+                    x.append(e.prename.toString().plus(" ").plus(e.lastname.toString()))
+                }
+            }
+            editText.setText(x.toString());
+            databaseHandler.close()
+        }
 
         var goToChildButton = findViewById<Button>(R.id.go_to_child_btn);
         goToChildButton.setOnClickListener{
@@ -42,21 +62,18 @@ class MainActivity : AppCompatActivity() {
             var lastname = findViewById<AutoCompleteTextView>(R.id.tx2);
             var editText = findViewById<EditText>(R.id.editText);
 
-            Toast.makeText(this,prename.editableText.toString().plus(" ").plus(lastname.editableText.toString()),Toast.LENGTH_SHORT).show()
-
             val databaseHandler: DatabaseHandler= DatabaseHandler(this)
             val emp:List<PersonModelClass> = databaseHandler.viewPerson()
-
             val x=StringBuilder()
+
             for(e in emp) {
-                //Toast.makeText(this,"saved:".plus(e.prename.plus(" ").plus(e.lastname)),Toast.LENGTH_SHORT).show();
                 x.append(e.prename.toString().plus(" ").plus(e.lastname.toString()))
                 x.append("\n")
             }
+
             editText.setText(x.toString());
             databaseHandler.addPerson(PersonModelClass(emp.size+1,prename.editableText.toString(),lastname.editableText.toString()));
             databaseHandler.close();
-
         }
     }
 }
